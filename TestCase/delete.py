b9f33f74e085414eb2814799fa7ed2af4d3d33e3
adeguy <3228532393@qq.com>
2023-11-07 21:58:36 +0800

    更新测试用例！

--- TestCase/delete.py
+++ TestCase/delete.py
@@ -6,12 +6,21 @@ base_URL = 'http://127.0.0.1:5000'
 case_data = DataFormZephyrScale(access_key)  # 输入许可码
 APItest = InterfaceTesting(base_URL)  # 输入根地址
 # 从用例管理平台获取数据
-data = case_data.data_form_zephyr_scale('HOR-T86')
+data = case_data.data_form_zephyr_scale('HOR-T89')
 # 对获得数据进行整理
 data_handed = case_data.data_handling(data)
 print(data_handed)
 # 进行接口测试
-response = APItest.send_post(endpoint=data_handed['endpoint'], header={data_handed["head"]}, data=data_handed['token'])
+response = APItest.send_post(
+    endpoint=data_handed['endpoint'],
+
+    data={
+            data_handed["id_pre_head"]: data_handed['id_head'],
+            data_handed['date_pre_head']: data_handed["date_head"],
+            data_handed["priority_pre_head"]: data_handed["priority_head"],
+            data_handed["content_pre_head"]:data_handed["content_head"]
+        }
+)
 # 进行断言
 Asser = Assertions(data_handed['状态码'])
 print(Asser.is_equal(response.status_code))  # 这里返回的是true和false
